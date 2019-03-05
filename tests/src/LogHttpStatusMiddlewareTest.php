425bfe52a64f8e60d2086a6a22c7237175d7bba1
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2019-03-05 12:00:19 +0100

    Make PSR-15 compliant

--- tests/src/LogHttpStatusMiddlewareTest.php
+++ tests/src/LogHttpStatusMiddlewareTest.php
@@ -3,9 +3,11 @@ namespace tests;
 
 use Germania\Middleware\LogHttpStatusMiddleware;
 use Psr\Log\LoggerInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Prophecy\Argument;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 class LogHttpStatusMiddlewareTest extends \PHPUnit\Framework\TestCase
 {
@@ -14,13 +16,15 @@ class LogHttpStatusMiddlewareTest extends \PHPUnit\Framework\TestCase
 	/**
 	 * @dataProvider provideCodesAndReason
 	 */
-	public function testSimple( $code, $reason, $status_message)
+	public function testDoublePass( $code, $reason, $status_message)
 	{
-		// Setup dependencies
+		// Setup SUT
 		$logger = $this->prophesize(LoggerInterface::class);
 		$logger->info( Argument::type("string"), [ 'status' => $status_message ])->shouldBeCalled();
 		$logger_mock = $logger->reveal();
+		$sut = new LogHttpStatusMiddleware( $logger_mock );
 
+		// Prepare PSR-7 stuff
 		$request = $this->prophesize(ServerRequestInterface::class);	
 		$request_mock = $request->reveal();
 
@@ -31,17 +35,48 @@ class LogHttpStatusMiddlewareTest extends \PHPUnit\Framework\TestCase
 
 		$next = function( $request, $response ) { return $response; };
 
-
-		// Setup SUT
-		$sut = new LogHttpStatusMiddleware( $logger_mock );
+		// Perform test
 		$result_response = $sut( $request_mock, $response_mock, $next);
-
-
-		// Eval
 		$this->assertEquals( $result_response, $response_mock);
 		$this->assertInstanceOf( ResponseInterface::class, $result_response);
 
 	}
+
+
+
+	/**
+	 * @dataProvider provideCodesAndReason
+	 */
+	public function testSinglePass( $code, $reason, $status_message)
+	{
+		// Setup SUT
+		$logger = $this->prophesize(LoggerInterface::class);
+		$logger->info( Argument::type("string"), [ 'status' => $status_message ])->shouldBeCalled();
+		$logger_mock = $logger->reveal();
+		$sut = new LogHttpStatusMiddleware( $logger_mock );
+
+
+		// Prepare PSR-7 stuff
+		$request = $this->prophesize( ServerRequestInterface::class );
+		$request_mock = $request->reveal();
+
+		$response = $this->prophesize(ResponseInterface::class);	
+		$response->getStatusCode()->willReturn( $code );	
+		$response->getReasonPhrase()->willReturn( $reason );	
+		$response_mock = $response->reveal();
+
+		$handler = $this->prophesize( RequestHandlerInterface::class );
+		$handler->handle( Argument::exact($request_mock) )->willReturn( $response_mock );
+		$handler_mock = $handler->reveal();
+
+
+		// Perform test
+		$result_response = $sut->process( $request_mock, $handler_mock);
+		$this->assertEquals( $result_response, $response_mock);
+		$this->assertInstanceOf( ResponseInterface::class, $result_response);		
+	}
+
+
 
 	public function provideCodesAndReason()
 	{
