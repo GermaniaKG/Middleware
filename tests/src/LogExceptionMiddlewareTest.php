38ecae24bde0f9062d1500d9d38b275b4c77d782
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2019-03-05 11:32:06 +0100

    Make PSR-15 compatible

--- tests/src/LogExceptionMiddlewareTest.php
+++ tests/src/LogExceptionMiddlewareTest.php
@@ -7,11 +7,49 @@ use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Prophecy\Argument;
 
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
 class LogExceptionMiddlewareTest extends \PHPUnit\Framework\TestCase
 {
 
+	public function testInstantiationAndInterfaces()
+	{
+		// Setup dependencies
+		$logger = $this->prophesize(LoggerInterface::class);
+		$logger_mock = $logger->reveal();
 
-	public function testSimple()
+		// Setup SUT
+		$sut = new LogExceptionMiddleware( $logger_mock );
+		$this->assertInstanceOf( MiddlewareInterface::class, $sut );
+
+	}
+
+
+	public function testSinglePassMiddlewareInterface()
+	{
+		// Setup dependencies
+		$logger = $this->prophesize(LoggerInterface::class);
+		$logger_mock = $logger->reveal();
+
+		$request = $this->prophesize( ServerRequestInterface::class );
+		$request_mock = $request->reveal();
+
+		$handler = $this->prophesize( RequestHandlerInterface::class );
+		$handler->handle( Argument::exact($request_mock) )->shouldBeCalled();
+		$handler_mock = $handler->reveal();
+
+		// Setup SUT
+		$sut = new LogExceptionMiddleware( $logger_mock );
+		$result = $sut->process( $request_mock, $handler_mock);
+
+		$this->assertInstanceOf( ResponseInterface::class, $result );
+
+	}
+
+
+	public function testDoublePassMiddlewareInterface()
 	{
 		$exception_message = "Yay!";
 
